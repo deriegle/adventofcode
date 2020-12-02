8112a0b8c80ef3ffe834bd353f872ab7bce481f7
Devin Riegle <riegledevin@gmail.com>
2020-12-02 08:54:53 -0500

    add day2 solution

---
+++ day2/index.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+const path = require('path');
+
+function parseFile() {
+  const lines = fs.readFileSync(path.join(__dirname, './input.txt'), 'utf-8').split('\n').filter(Boolean);
+
+  return lines.map((line) => {
+    if (!line) { return null; }
+    const [policy, letter, password] = line.split(' ');
+
+    return {
+      policy: policy.split('-'),
+      letter: letter[0],
+      password,
+    };
+  });
+}
+
+function part1() {
+  const passwords = parseFile();
+  const numberOfValidPasswords = passwords.reduce((acc, { policy, letter, password}) => {
+    const letterCount = password.split(letter).length - 1;
+    const isValid = letterCount >= policy[0] && letterCount <= policy[1];
+
+    if (isValid) {
+      acc += 1;
+    }
+
+    return acc;
+  }, 0);
+
+  console.log(numberOfValidPasswords);
+}
+
+function part2() {
+  const passwords = parseFile();
+  const numberOfValidPasswords = passwords.reduce((acc, { policy, letter, password}) => {
+    const [firstPosition, secondPosition] = policy;
+    const firstPositionHasCharacter = password[firstPosition - 1] === letter;
+    const secondPositionHasCharacter = password[secondPosition - 1] === letter;
+    const isValid = firstPositionHasCharacter && !secondPositionHasCharacter || !firstPositionHasCharacter && secondPositionHasCharacter;
+
+    if (isValid) {
+      acc += 1;
+    }
+
+    return acc;
+  }, 0);
+
+  console.log(numberOfValidPasswords);
+}
+
+part1();
+part2();
